77e6221cf934590e8c94d7a18613a11d6353e858
Natuski-Nakagawa <ronaldbongvquerubin@iskolarngbayan.pup.edu.ph>
2024-08-12 17:18:58 +0800

    created home page and added sql in database and image

--- client/pages/home.php
+++ client/pages/home.php
@@ -4,17 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home</title>
+    <link rel="stylesheet" href="../../server/style/trashpagemenu.css" >
 </head>
 <body>
-    <div class="container">
-        <div class="left">
-            asdfsadf
+    <div class="sidebar">
+        <h2>Noctyx</h2>
+        <a href="#home"  style="background-color: #8DA76E;">
+            <img src="../assets/homeicon.png" alt="Home Icon"> <!-- Home icon -->
+            Home
+        </a>
+        <a href="#trash">
+            <img src="../assets/trashicon.png" alt="Trash Icon"> <!-- Trash icon -->
+            Trash
+        </a>
+        <a href="#settings">
+            <img src="../assets/settingsicon.png" alt="Settings Icon"> <!-- Settings icon -->
+            Settings
+        </a>
+        <div class="footer">
+            <a href="#username" class="username-container">
+                <img src="../assets/usernameicon.png" alt="Username Icon"> <!-- Username icon -->
+                Username
+            </a>
         </div>
+    </div>
 
-        <div class="right">
-
-
+    <div class="content">
+        <div class="trash-icon">
+            <img src="../assets/notes.png" alt="Trash Icon"> <!-- Large trash icon -->
         </div>
+        <p>No notes</p>
     </div>
 </body>
 </html>
--- server/database/noctyx.sql
+++ server/database/noctyx.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 12, 2024 at 04:10 AM
+-- Generation Time: Aug 12, 2024 at 11:05 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `noctyx.db`
+-- Database: `noctyx`
 --
 
 -- --------------------------------------------------------
@@ -29,10 +29,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `notes` (
   `note id` int(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
+  `title` int(255) NOT NULL,
   `date created` timestamp(6) NOT NULL DEFAULT current_timestamp(6) ON UPDATE current_timestamp(6),
   `content` varchar(255) NOT NULL,
-  `user id` int(255) NOT NULL
+  `id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user id` int(255) NOT NULL,
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(12) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -40,20 +55,20 @@ CREATE TABLE `notes` (
 --
 
 --
--- Indexes for table `notes`
+-- Indexes for table `user`
 --
-ALTER TABLE `notes`
-  ADD KEY `fk_user` (`user id`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user id`);
 
 --
--- Constraints for dumped tables
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- Constraints for table `notes`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `notes`
-  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user id`) REFERENCES `user` (`user id`);
+ALTER TABLE `user`
+  MODIFY `user id` int(255) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
